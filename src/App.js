fd0098726bb4402f2494b9af017b322ae9c7b8a8
Jon Crowell <jon.crowell@stonegiantstudio.com>
2019-09-11 18:39:38 -0500

    Add another keypress handler to delete todo
    Backspace will delete an empty todo

--- src/App.js
+++ src/App.js
@@ -26,6 +26,20 @@ function App() {
     if (e.key === "Enter") {
       createTodoAtIndex(e, i);
     }
+    if (e.key === "Backspace" && todos[i].content === "") {
+      e.preventDefault();
+      return removeTodoAtIndex(i);
+    }
+  }
+
+  function removeTodoAtIndex(i) {
+    if (i === 0 && todos.length === 1) return;
+    setTodos(todos =>
+      todos.slice(0, i).concat(todos.slice(i + 1, todos.length))
+    );
+    setTimeout(() => {
+      document.forms[0].elements[i - 1].focus();
+    }, 0);
   }
 
   function createTodoAtIndex(e, i) {
