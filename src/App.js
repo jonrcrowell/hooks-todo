addf30761696dd8e2bbba47f02199916284c762b
Jon Crowell <jon.crowell@stonegiantstudio.com>
2019-09-11 18:24:53 -0500

    Refactor to merge two arrays rather than splicing
    Feels cleaner

--- src/App.js
+++ src/App.js
@@ -29,11 +29,14 @@ function App() {
   }
 
   function createTodoAtIndex(e, i) {
-    const newTodos = [...todos];
-    newTodos.splice(i + 1, 0, {
-      content: "",
-      isCompleted: false
-    });
+    const newTodo = [
+      {
+        content: "",
+        isCompleted: false
+      }
+    ];
+    const newTodos = [...todos, ...newTodo];
+
     setTodos(newTodos);
     setTimeout(() => {
       document.forms[0].elements[i + 1].focus();
